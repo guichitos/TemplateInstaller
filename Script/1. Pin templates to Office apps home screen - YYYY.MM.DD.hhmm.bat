aa42d55dd647be32a7637da9a8070557e8bb13de
guichitos <92882295+guichitos@users.noreply.github.com>
2025-11-20 16:39:36 -0400

    Instalador funcional 202520111639

---
+++ Script/1. Pin templates to Office apps home screen - YYYY.MM.DD.hhmm.bat
@@ -0,0 +1,61 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P1nEdIJ3PRoeH0xFggYsm7eKtZphuxzzJbjqijh05JCmxDHBAITAWORSOdWk/4/IXpIy5O7vKae6Zqb2R1RusCeA==
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P2jjkBCCbRrM/rxk01btT+fpAWhhGkiDNf2sk=
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P2jjkBAD3MsOXlxFAuYvfJWZcAmgmyy39cz73T
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P2jjkBBAHw6dzl3FEGYsn0YLIcm1O1xyU+
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P2jjkBGzbWq+DyzXgkd+rpY7Qcmwm+wyIQzKin9w==
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf7+61XloJyDRpeDozUt6ZdvvDA==
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFCt3cCuMOU+oD6MZqKW7yPiGpkwhdeU6dozS24i+Mu8X/0bnfDX+2EYK2OMJHglZcxuuYBs1ulIT+DTFFteQugzgSUGG6E4jJzU61yP5gjgvYd9pnswRkyS7vF3znqsE2HTzX7pOEWah7qpuMcoFwVr0SQnGk6VQRrbiZL7oDwqYY1saj2XRmIx5ooAiUTRwQAxlgrpj7xSpEcvqjiUPIDDKxA==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF25
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF25
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFCt3cCuMOU+oD6MZqKW7yPiGpkwhdeU6dozS24i+Mu8X/0bnfDX+2EYK2OMJHglZcxuuYBs1ulIT+DTFFteQugzgSUGG6E4jJzU61yP5gjgvYd9pnswRkyS7vF3znqsE2HTzX7pOEWah7qpuMcoFwVr0SQnGk6VQRrbiZL7oDwrxGlAmwl3uxadO0IslUi1JfhFUy61p7Dv0Qoa+zzdVJnPqourty1x0Zsrrf+QRhhCyhiJd3Ky2mR0KDXFcHFcSSxjFRTXYdw==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+setlocal enabledelayedexpansion
+
+set "ScriptDir=%~dp0"
+set "FILE=%AppData%\MainInstaller.bat"
+
+echo Ejecutando desde: "%ScriptDir%"
+if not exist "%FILE%" (
+    echo No se encontro el archivo en la carpeta del ejecutable.
+    echo Ruta esperada:
+    echo    %FILE%
+    pause
+    exit /b 1
+)
+
+echo Ejecutando:
+echo    "%FILE%" "%ScriptDir%"
+set "PIN_LAUNCHER_DIR=%ScriptDir%"
+call "%FILE%" "%ScriptDir%"
+pause
+endlocal
+exit /b 0
