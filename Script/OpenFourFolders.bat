36382f201ab0a52d9a446a3f25bf276eec271948
guichitos <92882295+guichitos@users.noreply.github.com>
2025-12-22 17:40:33 -0400

    Update folder opener to five paths

---
+++ Script/OpenFourFolders.bat
@@ -0,0 +1,12 @@
+@echo off
+set "FOLDER_ONE=C:\Users\PC\OneDrive\Desktop"
+set "FOLDER_TWO=Downloads"
+set "FOLDER_THREE=C:\Users\PC\OneDrive\Documentos"
+set "FOLDER_FOUR=C:\Users\PC\OneDrive\Pictures"
+set "FOLDER_FIVE=Inicio"
+
+start "" "%FOLDER_ONE%"
+start "" "%FOLDER_TWO%"
+start "" "%FOLDER_THREE%"
+start "" "%FOLDER_FOUR%"
+start "" "%FOLDER_FIVE%"
