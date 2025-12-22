1b4ffc10a952124a42e2716f399623ae12d67713
guichitos <92882295+guichitos@users.noreply.github.com>
2025-12-22 17:48:36 -0400

    Update folder list for folder opener script

---
+++ Script/OpenFourFolders.bat
@@ -0,0 +1,12 @@
+@echo off
+set "FOLDER_ONE=C:\Users\PC\AppData\Roaming\Microsoft\Templates\Document Themes\"
+set "FOLDER_TWO=C:\Users\PC\OneDrive\Documentos\Custom Office Templates\"
+set "FOLDER_THREE=C:\Users\PC\AppData\Roaming\Microsoft\Templates\"
+set "FOLDER_FOUR=C:\Users\PC\AppData\Roaming\Microsoft\Excel\XLSTART\"
+set "FOLDER_FIVE=C:\Users\PC\OneDrive\Documentos\Plantillas personalizadas de Office"
+
+start "" "%FOLDER_ONE%"
+start "" "%FOLDER_TWO%"
+start "" "%FOLDER_THREE%"
+start "" "%FOLDER_FOUR%"
+start "" "%FOLDER_FIVE%"
